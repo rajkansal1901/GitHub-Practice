cf61e0d70e5011364a24caa6541fe43bafbeee2c
Rajat Kansal <rajkansal1901@gmail.com>
2025-03-03 09:36:03 -0500

    First commit to demo page

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        int i = 43;
+        int j = 456;
+        System.out.println(Math.max(i, j));
+    }
+}
